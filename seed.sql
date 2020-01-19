5576502e24fbddc74289b6d2427c6cfbfbc874e6
Drew <andrewyoung4@mail.weber.edu>
2020-01-18 19:29:25 -0700

    creating first commit

---
+++ seed.sql
@@ -0,0 +1,14 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+("Red Lightsaber", "Toys", 19.99, 99),
+("Blue Lightsaber", "Toys", 19.99, 49),
+("The Child Plushie", "Collectibles", 14.99, 2),
+("Mandalorian Armor", "Costumes", 69.99, 15),
+("Darth Vader Mug", "Collectibles", 29.99, 6),
+("Chewbacca T-shirt", "Clothing", 9.99, 69),
+("Tatooine Sunset Poster", "Decorations", 24.99, 77),
+("BB-8 Roomba", "Appliances", 44.99, 33),
+("Death Star Poster", "Decorations", 19.99, 102),
+("Millenium Falcon Model", "Collectibles", 59.99, 21);
+
+SELECT * FROM products;
